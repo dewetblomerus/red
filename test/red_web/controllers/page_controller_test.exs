5452392486d3258658e698d4822204eb4ddfa50a
De Wet Blomerus <dewet.blomerus@salesloft.com>
2023-09-23 10:14:27 -0400

    Fix test ✅

--- test/red_web/controllers/page_controller_test.exs
+++ test/red_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule RedWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Ash Demo"
   end
 end
