fb11a33b3dc4fb5297cbc8e0b9e6c7dd39c938f9
De Wet Blomerus <dewet.blomerus@salesloft.com>
2023-09-26 07:32:28 -0400

    Auth0 almost working 🤷‍♂️

---
+++ lib/red/secrets.ex
@@ -0,0 +1,25 @@
+defmodule Red.Secrets do
+  use AshAuthentication.Secret
+
+  def secret_for([:authentication, :strategies, :auth0, :client_id], Red.Accounts.User, _) do
+    get_config(:client_id)
+  end
+
+  def secret_for([:authentication, :strategies, :auth0, :redirect_uri], Red.Accounts.User, _) do
+    get_config(:redirect_uri)
+  end
+
+  def secret_for([:authentication, :strategies, :auth0, :client_secret], Red.Accounts.User, _) do
+    get_config(:client_secret)
+  end
+
+  def secret_for([:authentication, :strategies, :auth0, :site], Red.Accounts.User, _) do
+    get_config(:site)
+  end
+
+  defp get_config(key) do
+    :red
+    |> Application.fetch_env!(:auth0)
+    |> Keyword.fetch(key)
+  end
+end
