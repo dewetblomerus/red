278c8bd2994485b303875e20d93c876df456a893
De Wet Blomerus <dewet.blomerus@salesloft.com>
2023-10-01 08:39:15 -0400

    Remove registry

--- lib/red/api/api.ex
+++ lib/red/api/api.ex
@@ -2,7 +2,7 @@ defmodule Red.Api do
   use Ash.Api
 
   resources do
+    resource Red.Api.Attempt
     resource Red.Api.User
-    registry Red.Api.Registry
   end
 end
--- lib/red/api/registry.ex
+++
@@ -1,8 +0,0 @@
-defmodule Red.Api.Registry do
-  use Ash.Registry
-
-  entries do
-    entry Red.Api.User
-    entry Red.Api.Attempt
-  end
-end
