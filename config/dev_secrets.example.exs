21b295764f52722e1c2e631090fbdab2001eccbe
De Wet Blomerus <dewet.blomerus@salesloft.com>
2023-10-29 09:04:56 -0400

    Clarify that https:// is needed 🔐

--- config/dev_secrets.example.exs
+++ config/dev_secrets.example.exs
@@ -5,5 +5,5 @@ config :red,
     client_id: "auth0-client-id",
     redirect_uri: "http://localhost:4000/auth",
     client_secret: "auth0-client-secret",
-    site: "auth0-domain"
+    site: "https://auth0-domain"
   }
