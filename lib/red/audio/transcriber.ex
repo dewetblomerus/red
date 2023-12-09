101587cf77958426d2c2c23b5d0c3f2cff05b2f3
De Wet Blomerus <dewet.blomerus@salesloft.com>
2023-12-09 11:43:06 -0500

    Break up big function ⚒️

--- lib/red/audio/transcriber.ex
+++ lib/red/audio/transcriber.ex
@@ -6,12 +6,33 @@ defmodule Red.Audio.Transcriber do
   @file_format "mp3"
 
   def transcribe(text) do
-    file_exists?(text)
+    if file_exists?(text) do
+      {:ok, :already_exists}
+    else
+      with {:ok, file_contents} <- perform_transcription(text),
+           upload_to_s3(text, file_contents) do
+        {:ok, :uploaded}
+      end
+    end
+  end
 
-    api_key = Application.fetch_env!(:red, :open_api_key)
+  def file_exists?(text) do
+    perform_check =
+      S3.head_object(
+        "spellsightwords",
+        "audio/#{Slugger.file_name(text, @file_format)}"
+      )
+      |> ExAws.request()
 
+    case perform_check do
+      {:ok, %{status_code: 200}} -> true
+      {:error, {:http_error, 404, _}} -> false
+    end
+  end
+
+  def perform_transcription(text) do
     headers = [
-      {"Authorization", "Bearer #{api_key}"},
+      {"Authorization", "Bearer #{openapi_key()}"},
       {"Content-Type", "application/json"}
     ]
 
@@ -28,6 +49,10 @@ defmodule Red.Audio.Transcriber do
         headers: headers
       )
 
+    {:ok, file_contents}
+  end
+
+  def upload_to_s3(text, file_contents) do
     %{status_code: 200} =
       S3.put_object(
         "spellsightwords",
@@ -37,17 +62,7 @@ defmodule Red.Audio.Transcriber do
       |> ExAws.request!()
   end
 
-  def file_exists?(text) do
-    perform_check =
-      S3.head_object(
-        "spellsightwords",
-        "audio/#{Slugger.file_name(text, @file_format)}"
-      )
-      |> ExAws.request()
-
-    case perform_check do
-      {:ok, %{status_code: 200}} -> true
-      {:error, {:http_error, 404, _}} -> false
-    end
+  defp openapi_key do
+    Application.fetch_env!(:red, :open_api_key)
   end
 end
