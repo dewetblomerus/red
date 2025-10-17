b9410e02a0720f4bdb4a602471ebd59d3651e1fd
De Wet Blomerus <dewetblomerus@gmail.com>
2025-10-16 23:01:38 -0400

    SSL verify none

--- config/runtime.exs
+++ config/runtime.exs
@@ -40,7 +40,7 @@ if config_env() == :prod do
     if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :red, Red.Repo,
-    # ssl: true,
+    ssl: [verify: :verify_none],
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6
